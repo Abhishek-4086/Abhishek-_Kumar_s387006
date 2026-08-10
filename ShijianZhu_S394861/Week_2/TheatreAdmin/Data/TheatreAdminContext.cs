dc409cb3b644222801b7a147f361778a47db26a1
Hulandaoke <110921121+Hulandaoke@users.noreply.github.com>
2026-08-10 20:45:10 +0930

    Scaffold movie and category CRUD workflows

--- ShijianZhu_S394861/Week_2/TheatreAdmin/Data/TheatreAdminContext.cs
+++ ShijianZhu_S394861/Week_2/TheatreAdmin/Data/TheatreAdminContext.cs
@@ -21,6 +21,34 @@ public class TheatreAdminContext(DbContextOptions<TheatreAdminContext> options)
             .HasForeignKey(movie => movie.CategoryId)
             .IsRequired()
             .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action", Code = "ACT" },
+            new Category { Id = 2, Name = "Drama", Code = "DRM" },
+            new Category { Id = 3, Name = "Horror", Code = "HOR" }
+        );
+
+        modelBuilder.Entity<Movie>().HasData(
+            new Movie
+            {
+                Id = 1,
+                Name = "Seven Samurai",
+                ReleaseDate = new DateTime(1954, 4, 26),
+                Director = "Akira Kurosawa",
+                ContactEmail = "programming@theatre.example",
+                Language = MovieLanguage.Japanese,
+                CategoryId = 1
+            },
+            new Movie
+            {
+                Id = 2,
+                Name = "Farewell My Concubine",
+                ReleaseDate = new DateTime(1993, 1, 1),
+                Director = "Chen Kaige",
+                ContactEmail = "programming@theatre.example",
+                Language = MovieLanguage.Chinese,
+                CategoryId = 2
+            }
+        );
     }
 }
-
--- ShijianZhu_S394861/Week_2/TheatreAdmin/Program.cs
+++ ShijianZhu_S394861/Week_2/TheatreAdmin/Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddDbContext<TheatreAdminContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<TheatreAdminContext>();
+    await context.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
